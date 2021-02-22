05e4185d2289a0789c5a04d2aec6805891750718
David Forrest <drf5na@gmail.com>
2021-02-22 09:43:09 -0500

    twiddle.py: Add repeat+interval for groups of rings

--- twiddle.py
+++ twiddle.py
@@ -7,14 +7,18 @@ parser.add_argument("--pulses",help="pulses to send", default=1, type=int)
 parser.add_argument("--gpio",help="gpio pin", default=14, type=int)
 parser.add_argument("--on",help="on time (ms)", default=250, type=int)
 parser.add_argument("--delay",help="delay time (ms)", default=250, type=int)
+parser.add_argument("--repeat",help="repetition groups", default=1, type=int)
+parser.add_argument("--interval",help="delay time (ms)", default=500, type=int)
 args = parser.parse_args()
-
 
 bell = gpiozero.Buzzer(args.gpio)
 
-for pulse in range(args.pulses):
-    bell.on()
-    time.sleep(args.on/1000.0)
-    bell.off()
-    time.sleep(args.delay/1000.0)
-
+for rep in range(args.repeat):
+    if rep > 0:
+        time.sleep(args.interval/1000.0)
+    for pulse in range(args.pulses):
+        if pulse > 0: 
+            time.sleep(args.delay/1000.0)
+        bell.on()
+        time.sleep(args.on/1000.0)
+        bell.off()
